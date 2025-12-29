1e23b427dcbac52ceafc22773bd288b432102346
Andrea Biondini <abiondini73@gmail.com>
2025-12-29 16:24:13 +0100

    Primo abbozzo del metodo getMentoriDisponibili() della classe MentorManager.

--- src/main/java/it/unicam/ids2026/hackhub/managers/MentorManager.java
+++ src/main/java/it/unicam/ids2026/hackhub/managers/MentorManager.java
@@ -3,13 +3,14 @@ package it.unicam.ids2026.hackhub.managers;
 import it.unicam.ids2026.hackhub.roles.Mentore;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class MentorManager {
 
     private Collection<Mentore> listaMentori;
 
     public Collection<Mentore> getMentoriDisponibili() {
-        return listaMentori;
+        return listaMentori.stream().filter(m -> m.getAssociatedHackathon() == null).collect(Collectors.toCollection());
     }
 
 }
