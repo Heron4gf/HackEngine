b479396932c516f7bb31441f34d6e3a911515ff1
Manu24life <pasquinimanuel24@gmail.com>
2025-12-29 16:35:13 +0100

    Stato Hackathon

--- src/main/java/it/unicam/ids2026/hackhub/data/StatoHackathon.java
+++ src/main/java/it/unicam/ids2026/hackhub/data/StatoHackathon.java
@@ -1,5 +1,13 @@
 package it.unicam.ids2026.hackhub.data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor // genera il costruttore per i final
 public enum StatoHackathon {
-
+    ISCRIZIONE("Fase d'iscrzione"),
+    IN_CORSO("Fase di svolgimento"),
+    IN_VALUTAZIONE("Fase di valutazione"),
+    CONCLUSO("Concluso"),
+    private final String state;
 }
