0d25af4e69dba66a62f9a45f326357989ed7d59b
Heron4gf <herongiambuzzi@gmail.com>
2025-12-29 15:24:52 +0100

    HackHub is now singleton

--- src/main/java/it/unicam/ids2026/hackhub/HackHub.java
+++ src/main/java/it/unicam/ids2026/hackhub/HackHub.java
@@ -1,4 +1,17 @@
 package it.unicam.ids2026.hackhub;
 
 public class HackHub {
+
+    private static HackHub instance;
+
+    private HackHub() {
+        // using Singleton
+    }
+
+    public static HackHub getInstance() {
+        if(instance == null) instance = new HackHub();
+        return instance;
+    }
+
+
 }
--- src/main/java/it/unicam/ids2026/hackhub/roles/Giudice.java
+++ src/main/java/it/unicam/ids2026/hackhub/roles/Giudice.java
@@ -1,4 +1,7 @@
 package it.unicam.ids2026.hackhub.roles;
 
+import lombok.Data;
+
+@Data
 public class Giudice {
 }
