13d02801c5b1c2478a88e066a2ffb79312f429be
Andrea Biondini <abiondini73@gmail.com>
2026-01-02 18:07:57 +0100

    Aggiunta di notazioni lombok nella classe MentorManager e revisione della logica del metodo getMentoriDisponibili

--- src/main/java/it/unicam/ids2026/hackhub/managers/MentorManager.java
+++ src/main/java/it/unicam/ids2026/hackhub/managers/MentorManager.java
@@ -1,16 +1,21 @@
 package it.unicam.ids2026.hackhub.managers;
 
+import it.unicam.ids2026.hackhub.Hackathon;
 import it.unicam.ids2026.hackhub.roles.Mentore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
 
 import java.util.Collection;
 import java.util.stream.Collectors;
 
+@AllArgsConstructor
+@Getter
 public class MentorManager {
-
     private Collection<Mentore> listaMentori;
 
-    public Collection<Mentore> getMentoriDisponibili() {
-        return listaMentori.stream().filter(m -> m.getAssociatedHackathon() == null).collect(Collectors.toCollection());
+    @
+    public Collection<Mentore> getMentoriDisponibili(@NonNull Hackathon h) {
+        return listaMentori.stream().filter(m -> !h.getMentori().contains(m)).collect(Collectors.toList());
     }
-
 }
