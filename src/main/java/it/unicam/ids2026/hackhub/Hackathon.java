c52996608334bc58f65a15b833d7feb030e6cfaa
Heron4gf <herongiambuzzi@gmail.com>
2026-01-02 17:59:45 +0100

    integrated lombok for code clarity, improved Hackathon implementation

--- src/main/java/it/unicam/ids2026/hackhub/Hackathon.java
+++ src/main/java/it/unicam/ids2026/hackhub/Hackathon.java
@@ -1,31 +1,54 @@
 package it.unicam.ids2026.hackhub;
 
+import it.unicam.ids2026.hackhub.data.DatiHackathon;
 import it.unicam.ids2026.hackhub.data.Intervallo;
 import it.unicam.ids2026.hackhub.data.Sottomissione;
 import it.unicam.ids2026.hackhub.data.StatoHackathon;
-import it.unicam.ids2026.hackhub.managers.HackathonManager;
-import it.unicam.ids2026.hackhub.managers.MentorManager;
+import it.unicam.ids2026.hackhub.roles.Giudice;
 import it.unicam.ids2026.hackhub.roles.Mentore;
+import it.unicam.ids2026.hackhub.roles.Organizzatore;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.UUID;
 
+@RequiredArgsConstructor
 public class Hackathon {
 
-    private StatoHackathon stato;
+    @NonNull
+    private final Organizzatore organizzatore;
+
+    @NonNull
+    private final DatiHackathon datiHackathon;
+
+    @NonNull
+    private final Collection<Mentore> mentori;
+
+    @NonNull
+    private final Giudice giudice;
+
     @Getter
-    private UUID id;
+    @NonNull
+    private final Intervallo periodoIscrizioni;
     @Getter
-    private Intervallo periodoIscrizioni;
+    @NonNull
+    private final Intervallo durataHackathon;
+
+    private StatoHackathon stato = StatoHackathon.ISCRIZIONE;
+    private final Collection<Sottomissione> sottomissioni = new LinkedList<>();
+
     @Getter
-    private Intervallo durataHackathon;
+    private final UUID id = UUID.randomUUID();
 
     public void aggiungiMentore(Mentore mentore) {
-
+        this.mentori.add(mentore);
     }
 
     public void aggiungiSottomissione(Sottomissione sottomissione) {
-
+        this.sottomissioni.add(sottomissione);
     }
 
     private StatoHackathon cambiaStato(StatoHackathon nuovoStato) {
