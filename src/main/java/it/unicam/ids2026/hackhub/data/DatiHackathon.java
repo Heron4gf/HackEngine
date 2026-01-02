d52682b1cfc64115aff4d87e897380e8c300b10d
Manu24life <pasquinimanuel24@gmail.com>
2026-01-02 17:44:29 +0100

    Dati Hackathon

--- src/main/java/it/unicam/ids2026/hackhub/data/DatiHackathon.java
+++ src/main/java/it/unicam/ids2026/hackhub/data/DatiHackathon.java
@@ -1,4 +1,17 @@
 package it.unicam.ids2026.hackhub.data;
 
 public class DatiHackathon {
+    private String nome;
+    private String luogo;
+    // private BigDecimal premioInDenaro; Da controllare
+    private int dimensioneMaxTeam;
+    private String regolamento;
+    public DatiHackathon(String nome, String luogo,
+                         int dimensioneMaxTeam, String regolamento) {
+        this.nome = nome;
+        this.luogo = luogo;
+        // this.premioInDenaro = premioInDenaro;
+        this.dimensioneMaxTeam = dimensioneMaxTeam;
+        this.regolamento = regolamento;
+    }
 }
