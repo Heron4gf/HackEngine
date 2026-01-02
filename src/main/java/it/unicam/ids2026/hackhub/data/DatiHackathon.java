34db791e916d09fdd74b4367e427ab619ada77fd
Manu24life <pasquinimanuel24@gmail.com>
2026-01-02 17:58:12 +0100

    Dati Hackathon aggiunta dei Getter e Setter

--- src/main/java/it/unicam/ids2026/hackhub/data/DatiHackathon.java
+++ src/main/java/it/unicam/ids2026/hackhub/data/DatiHackathon.java
@@ -1,10 +1,19 @@
 package it.unicam.ids2026.hackhub.data;
+import lombok.Getter;
+import lombok.Setter;
 
 public class DatiHackathon {
+    @Getter
     private String nome;
+    @Getter
     private String luogo;
-    // private BigDecimal premioInDenaro; Da controllare
+    /* private BigDecimal premioInDenaro;
+     Da controllare se poi si decide di implementare la classe
+     Money
+     */
+    @Getter
     private int dimensioneMaxTeam;
+    @Getter @Setter
     private String regolamento;
     public DatiHackathon(String nome, String luogo,
                          int dimensioneMaxTeam, String regolamento) {
