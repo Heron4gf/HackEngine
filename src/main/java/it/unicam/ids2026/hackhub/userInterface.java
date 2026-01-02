bf04407bfd4f6ede350c448c44a646e15802ed46
Andrea Biondini <abiondini73@gmail.com>
2026-01-02 18:38:02 +0100

    Added interface userInterface

---
+++ src/main/java/it/unicam/ids2026/hackhub/userInterface.java
@@ -0,0 +1,16 @@
+package it.unicam.ids2026.hackhub;
+
+import it.unicam.ids2026.hackhub.data.DatiHackathon;
+import it.unicam.ids2026.hackhub.roles.Mentore;
+
+import java.util.Collection;
+
+public interface userInterface {
+    Collection<Mentore> richiestaMentoriDisponibili();
+
+    //Review return type
+    Hackathon createHackathon(DatiHackathon datiHackathon);
+
+    //Review return type
+    Collection<Mentore> selezionaMentori(Collection<Mentore> mentori);
+}
