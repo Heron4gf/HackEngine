36497d223b4cb121a773f97a4b91b12f5900b034
Heron4gf <herongiambuzzi@gmail.com>
2026-01-20 17:27:32 +0100

    changed contract of MembroStaff

--- src/main/java/it/unicam/ids2026/hackhub/roles/staff/MembroStaff.java
+++ src/main/java/it/unicam/ids2026/hackhub/roles/staff/MembroStaff.java
@@ -1,7 +1,10 @@
 package it.unicam.ids2026.hackhub.roles.staff;
 
+import it.unicam.ids2026.hackhub.hackathon.Hackathon;
 import it.unicam.ids2026.hackhub.roles.User;
 
 public interface MembroStaff extends User {
     String getCognome();
+    Hackathon getAssociatedHackathon();
+    void setAssociatedHackathon(Hackathon hackathon);
 }
