ce11886b44af8f9b50b4faae43a2b1720789290d
Manu24life <pasquinimanuel24@gmail.com>
2026-01-20 13:07:34 +0100

    UserManager

---
+++ src/main/java/it/unicam/ids2026/hackhub/managers/UserManager.java
@@ -0,0 +1,9 @@
+package it.unicam.ids2026.hackhub.managers;
+
+import it.unicam.ids2026.hackhub.roles.Utente;
+
+import java.util.Set;
+
+public class UserManager {
+    private Set<Utente> users;
+}
