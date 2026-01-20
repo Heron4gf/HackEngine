3ae8bfbec7c8c911f9733c4fc346f5f931d1c4de
Manu24life <pasquinimanuel24@gmail.com>
2026-01-20 17:12:19 +0100

    Classe Utente

--- src/main/java/it/unicam/ids2026/hackhub/roles/team/Utente.java
+++ src/main/java/it/unicam/ids2026/hackhub/roles/team/Utente.java
@@ -1,10 +1,34 @@
 package it.unicam.ids2026.hackhub.roles.team;
 
+import it.unicam.ids2026.hackhub.roles.invito.Invito;
+
+import java.util.Collection;
 import java.util.UUID;
 
 public class Utente extends AbstractUser {
+    private Team teamCorrente;
+    private Collection<Invito> casella_inviti;
 
     public Utente(UUID id, String nome) {
         super(id, nome);
+    }
+
+    public Team creaTeam(String nome, int maxMembri) {
+        Team team = new Team(nome, maxMembri);
+        team.aggiungiUtenti(this);
+        return team;
+    }
+
+    public void riceviInvito(Invito invito) {
+        if (!haTeam()) casella_inviti.add(invito);
+        throw new IllegalArgumentException("l'utente "+this.getNome()+" ha già un team");
+    }
+
+    public void setTeam(Team team) {
+        this.teamCorrente = team;
+    }
+
+    public boolean haTeam() {
+        return this.teamCorrente != null;
     }
 }
