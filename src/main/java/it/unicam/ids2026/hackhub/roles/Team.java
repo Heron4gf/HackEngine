d1cbd7e0125c26c0704003efa34d8d84d1555c30
Andrea Biondini <abiondini73@gmail.com>
2026-01-20 16:27:26 +0100

    First draft of Team and refinement of TeamManager

--- src/main/java/it/unicam/ids2026/hackhub/roles/Team.java
+++ src/main/java/it/unicam/ids2026/hackhub/roles/Team.java
@@ -1,14 +1,43 @@
 package it.unicam.ids2026.hackhub.roles;
+import it.unicam.ids2026.hackhub.HackHub;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.HashSet;
 import java.util.Set;
 
-@RequiredArgsConstructor
+
 @Getter
 public class Team {
-    private final int maxMembri;
-    private final String nome;
-
+    private int maxMembri;
+    private String nome;
+    private int numeroMembri = 0;
     private Set<Utente> membri;
+
+    public Team(@NotNull String nome, int maxMembri) {
+        this.nome = nome;
+        this.maxMembri = maxMembri;
+    }
+
+
+    void aggiungiUtenti(@NotNull Utente utente) {
+        if (numeroMembri <= maxMembri) {
+            membri.add(utente);
+            numeroMembri++;
+        }
+        else {
+            throw new IllegalArgumentException("Numero massimo superato");
+        }
+    }
+
+    void esciDalTeam(Utente utente) {
+        membri.remove(utente);
+        numeroMembri--;
+        if (numeroMembri == 0) {
+            HackHub.getInstance().getAllTeams().remove(this);
+        }
+    }
+
+
+
 }
