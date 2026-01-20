f66324cdc94c11f46c9b7efa107093d679393574
Heron4gf <herongiambuzzi@gmail.com>
2026-01-20 17:21:53 +0100

    modified UserManager to use User instead of Utente

--- src/main/java/it/unicam/ids2026/hackhub/managers/UserManager.java
+++ src/main/java/it/unicam/ids2026/hackhub/managers/UserManager.java
@@ -1,11 +1,11 @@
 package it.unicam.ids2026.hackhub.managers;
 
-import it.unicam.ids2026.hackhub.roles.team.Utente;
+import it.unicam.ids2026.hackhub.roles.User;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Set;
 
 @RequiredArgsConstructor
 public class UserManager {
-    private final Set<Utente> users;
+    private final Set<User> users;
 }
