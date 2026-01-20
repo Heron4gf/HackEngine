e23f568a7b127c270ec16027baf3e4c1d34ce365
Manu24life <pasquinimanuel24@gmail.com>
2026-01-20 15:44:54 +0100

    UserManager

--- src/main/java/it/unicam/ids2026/hackhub/status/StatoInValutazione.java
+++ src/main/java/it/unicam/ids2026/hackhub/status/StatoInValutazione.java
@@ -9,6 +9,8 @@ public class StatoInValutazione implements StatoHackathon {
     @Override
     public void next(Hackathon hackathon) {
 
+            hackathon.setState(new StatoConcluso());
+
     }
 
     @Override
@@ -18,11 +20,11 @@ public class StatoInValutazione implements StatoHackathon {
 
     @Override
     public void aggiungiMentore(Hackathon hackathon, Mentore mentore) {
-
+        throw new IllegalStateException("Impossibile aggiungere membri durante la fase di valutazione");
     }
 
     @Override
     public void aggiungiSottomissione(Hackathon hackathon, Sottomissione sottomissione) {
-
+        throw new IllegalStateException("Impossibile sottomettere progetti durante la fase di iscrizione");
     }
 }
