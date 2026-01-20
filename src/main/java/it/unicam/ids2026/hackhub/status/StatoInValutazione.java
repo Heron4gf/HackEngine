751d84aaf7554405ec87b70984c6372c5ae33c79
Heron4gf <herongiambuzzi@gmail.com>
2026-01-20 12:34:01 +0100

    aggiunte classi di stato

---
+++ src/main/java/it/unicam/ids2026/hackhub/status/StatoInValutazione.java
@@ -0,0 +1,28 @@
+package it.unicam.ids2026.hackhub.status;
+
+import it.unicam.ids2026.hackhub.Hackathon;
+import it.unicam.ids2026.hackhub.data.Sottomissione;
+import it.unicam.ids2026.hackhub.roles.Mentore;
+import it.unicam.ids2026.hackhub.roles.Team;
+
+public class StatoInValutazione implements StatoHackathon {
+    @Override
+    public void next(Hackathon hackathon) {
+
+    }
+
+    @Override
+    public void iscriviTeam(Hackathon hackathon, Team team) {
+
+    }
+
+    @Override
+    public void aggiungiMentore(Hackathon hackathon, Mentore mentore) {
+
+    }
+
+    @Override
+    public void aggiungiSottomissione(Hackathon hackathon, Sottomissione sottomissione) {
+
+    }
+}
