0ba9261bd1dacb6d16454a98995de8465da6cf04
Heron4gf <herongiambuzzi@gmail.com>
2026-01-22 16:24:23 +0100

    minor refactor

--- src/main/java/it/unicam/ids2026/hackhub/HackHub.java
+++ src/main/java/it/unicam/ids2026/hackhub/HackHub.java
@@ -1,15 +1,6 @@
 package it.unicam.ids2026.hackhub;
 
-import it.unicam.ids2026.hackhub.hackathon.data.DatiHackathon;
-import it.unicam.ids2026.hackhub.hackathon.Hackathon;
-import it.unicam.ids2026.hackhub.hackathon.data.Intervallo;
 import it.unicam.ids2026.hackhub.managers.*;
-import it.unicam.ids2026.hackhub.roles.staff.Giudice;
-import it.unicam.ids2026.hackhub.roles.staff.Mentore;
-import it.unicam.ids2026.hackhub.roles.staff.Organizzatore;
-import it.unicam.ids2026.hackhub.roles.team.Team;
-import it.unicam.ids2026.hackhub.roles.team.Utente;
-import lombok.NonNull;
 
 import java.util.*;
 
