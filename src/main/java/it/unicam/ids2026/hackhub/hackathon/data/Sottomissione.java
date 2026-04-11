f07f8e9f7c7d81b66aab85c70c94a1b5737c6a61
Manu24life <pasquinimanuel24@gmail.com>
2026-04-11 11:38:53 +0200

    Branch sbagliato sottomisione si trova in iterazione 3

--- src/main/java/it/unicam/ids2026/hackhub/hackathon/data/Sottomissione.java
+++ src/main/java/it/unicam/ids2026/hackhub/hackathon/data/Sottomissione.java
@@ -1,14 +1,5 @@
 package it.unicam.ids2026.hackhub.hackathon.data;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import java.io.File;
 
-@Data
-@AllArgsConstructor
 public class Sottomissione {
-
-    private String nome;
-    private String descrizione;
-    private File allegato;
 
 }
