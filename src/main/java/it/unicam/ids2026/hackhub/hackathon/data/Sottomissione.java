5bd12ebfa5fb94b7414344822653c475a56e3df8
Manu24life <pasquinimanuel24@gmail.com>
2026-04-11 11:33:26 +0200

    Classe Sottomissione con i relativi getter e setter tramite notazione lombok java

--- src/main/java/it/unicam/ids2026/hackhub/hackathon/data/Sottomissione.java
+++ src/main/java/it/unicam/ids2026/hackhub/hackathon/data/Sottomissione.java
@@ -1,4 +1,14 @@
 package it.unicam.ids2026.hackhub.hackathon.data;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.io.File;
 
+@Data
+@AllArgsConstructor
 public class Sottomissione {
+
+    private String nome;
+    private String descrizione;
+    private File allegato;
+
 }
