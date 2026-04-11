48731d437807ffceaac5e1b52e79809cfb8e5a74
Heron4gf <herongiambuzzi@gmail.com>
2026-04-11 11:30:52 +0200

    added javadoc and constructor in EventListener

--- src/main/java/it/unicam/ids2026/core/events/EventPublisher.java
+++ src/main/java/it/unicam/ids2026/core/events/EventPublisher.java
@@ -3,22 +3,35 @@ package it.unicam.ids2026.core.events;
 import it.unicam.ids2026.api.events.DeletionListener;
 import it.unicam.ids2026.api.events.Listener;
 import it.unicam.ids2026.core.roles.team.Team;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 import java.util.Collection;
+import java.util.LinkedList;
 
+@AllArgsConstructor
+@NoArgsConstructor
 public class EventPublisher {
-    private Collection<Listener> listeners;
+    @NonNull private Collection<Listener> listeners = new LinkedList<>();
 
+    /**
+     * @param listener
+     */
     public void registerListener(@NonNull Listener listener) {
         listeners.add(listener);
     }
 
+    /**
+     * @param listener
+     */
     public void deleteListener(@NonNull Listener listener) {
-        listeners.add(listener);
+        listeners.remove(listener);
     }
 
-
+    /**
+     * @param team
+     */
     public void publishDeletion(@NonNull Team team) {
         listeners.stream()
                 .filter(DeletionListener.class::isInstance)
