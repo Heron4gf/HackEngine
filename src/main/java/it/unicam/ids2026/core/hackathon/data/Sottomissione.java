696e878bf96c511819f8363312fafc766d32bcab
Manu24life <pasquinimanuel24@gmail.com>
2026-04-11 11:45:13 +0200

    classe sottomissione con costruttore getter e setter

--- src/main/java/it/unicam/ids2026/core/hackathon/data/Sottomissione.java
+++ src/main/java/it/unicam/ids2026/core/hackathon/data/Sottomissione.java
@@ -1,4 +1,20 @@
 package it.unicam.ids2026.core.hackathon.data;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
+import java.io.File;
+import java.util.Objects;
+@Data
+@RequiredArgsConstructor
 public class Sottomissione {
+
+   @NonNull
+   private String nome;
+   @NonNull
+   private String descrizione;
+   @NonNull
+   private File allegato;
+
+
 }
