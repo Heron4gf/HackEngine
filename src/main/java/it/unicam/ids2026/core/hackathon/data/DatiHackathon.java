7e737f3e79ce03c5a6a0f523f8a892c9bb210e6c
Manu24life <pasquinimanuel24@gmail.com>
2026-04-11 12:22:37 +0200

    Aggiunta Jackarta e correzione delle parti LomBok in dati Hackathon

--- src/main/java/it/unicam/ids2026/core/hackathon/data/DatiHackathon.java
+++ src/main/java/it/unicam/ids2026/core/hackathon/data/DatiHackathon.java
@@ -3,11 +3,32 @@ package it.unicam.ids2026.core.hackathon.data;
 import java.math.BigDecimal;
 import java.util.Currency;
 
+import lombok.NonNull;
+
+import jakarta.validation.constraints.*;
+
 public record DatiHackathon(
+
+        @NonNull
+        @NotBlank(message = "Il nome non può essere vuoto")
         String nome,
+
+        @NonNull
+        @NotBlank(message = "Il luogo non può essere vuoto")
         String luogo,
+
+        @NonNull
+        @DecimalMin(value = "0.0", inclusive = false, message = "Il premio deve essere maggiore di 0")
         BigDecimal premioInDenaro,
+
+        @NonNull
         Currency currency,
+
+        @Min(value = 1, message = "Il team deve avere almeno 1 partecipante")
+        @Max(value = 20, message = "Il team può avere al massimo 20 partecipanti")
         int dimensioneMaxTeam,
+
+        @NonNull
+        @NotBlank(message = "Il regolamento non può essere vuoto")
         String regolamento
 ) {}
