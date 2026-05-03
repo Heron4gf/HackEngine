df28564a1eb3c1c04f3dd82684908c80caacfd0f
Heron4gf <herongiambuzzi@gmail.com>
2026-05-03 12:41:48 +0200

    fixed a set

--- src/main/java/it/unicam/ids2026/core/managers/ViolationManager.java
+++ src/main/java/it/unicam/ids2026/core/managers/ViolationManager.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
+import java.util.Set;
 import java.util.UUID;
 
 /**
